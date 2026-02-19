14ff59a96f1ab382346eec8ba9db84bc4e32d70a
Azka <azka.adziman@gmail.com>
2026-02-19 23:54:56 +0000

    feat: add public getter functions to Node class

--- Node.h
+++ Node.h
@@ -7,6 +7,18 @@ class Node {
     public:
         Node(int id, std::string name, bool isStore = false)
             : id(id), name(name), isStore(isStore) {}
+
+    // add public getter functions
+    int getId() const {
+        return id;
+    } 
+    std::string getName() const {
+        return name;
+    }
+    bool getIsStore() const {
+        return isStore;
+    }
+    
     private:
     int id;
     std::string name;
