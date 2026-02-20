30b76ee823d1b76239ea4776a13ad4a03e3eb71e
Azka <azka.adziman@gmail.com>
2026-02-20 00:22:49 +0000

    feat: initialize Map class with nodes for Store and Houses

--- src/Map.cpp
+++ src/Map.cpp
@@ -3,6 +3,17 @@
 #include <algorithm>
 #include <random>
 
+Map::Map() {
+    std::random_device rd;
+    generator.seed(rd());
+
+    // initialise nodes
+    nodes.push_back(Node(0, "Store", true));
+    for (int i = 1; i <= 10; ++i) {
+        nodes.push_back(Node(i, "House " + std::to_string(i)));
+    }
+}
+
 void Map::addEdge(int a, int b) {
     // create a random distance
     std::uniform_real_distribution<double> distribution(0.1,1.0);
